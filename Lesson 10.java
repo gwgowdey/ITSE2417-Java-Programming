212adbed40bffe7fa4c19ea2dab19cb73db13a0b
Griffin Gowdey <gwgowdey@gmail.com>
2021-01-20 21:52:31 -0600

    Interfaces.

---
+++ Lesson 10.java
@@ -0,0 +1,61 @@
+// Student: Griffin Gowdey.
+// Instructor: Daniel Goodman.
+// Class Number: ITSE1479-86039.
+// Class Name: Java Programming.
+// Semester: Fall 2020.
+// Lesson 10.
+
+/*
+Assignment:
+Add static methods largest and smallest to the Measurable interface. 
+The methods should return the object with the largest or smallest 
+measure from an array of Measurable objects.
+*/
+
+public class Lesson10
+{
+   /*
+      Computes the average of the measures of the given objects.
+      @param objects an array of Measurable objects.
+      @return the average of the measures.
+   */
+   public static double average(Measurable[] objects)
+   {
+      double sum = 0;
+      for (Measurable obj : objects)
+      {
+         sum = sum + obj.getMeasure();
+      }
+      if (objects.length > 0)
+      {
+         return sum / objects.length;
+      }
+      else
+      {
+         return 0;
+      }
+   }
+
+   /*
+      Computes the maximum of the measures of the given objects.
+      @param objects an array of Measurable objects.
+      @return the maximum of the measures, null if array is empty.
+   */
+   public static Measurable max(Measurable[] objects)
+   {
+      if (objects.length == 0)
+      {
+         return null;
+      }
+      Measurable max = objects[0];
+      for (Measurable obj : objects)
+      {
+         if (obj.getMeasure() > max.getMeasure())
+         {
+            max = obj;
+         }
+      }
+
+      return max;
+   }
+}

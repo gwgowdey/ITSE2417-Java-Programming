080602b28a85ca013204f336c852984d27ac00be
Griffin Gowdey <gwgowdey@gmail.com>
2021-01-20 21:42:36 -0600

    Introduction.

---
+++ Lesson 1.java
@@ -0,0 +1,20 @@
+// Student: Griffin Gowdey.
+// Instructor: Daniel Goodman.
+// Class Number: ITSE1479-86039.
+// Class Name: Java Programming.
+// Semester: Fall 2020.
+// Lesson 1.
+
+/*
+Assignment:
+Write a program that prints a greeting of your choice, 
+perhaps in a language other than English.
+*/
+
+public class Lesson1 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello, world!");
+	}
+}
